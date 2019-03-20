983a26feac130de65d896c1a2c0db930c696a6cb
Vlad Tsirkunov <vladcirkunov598@gmail.com>
2019-03-20 22:56:57 +0300

    GetUserCoordinates

--- sources/components/MapWorker.js
+++ sources/components/MapWorker.js
@@ -3,11 +3,16 @@ import { CardItem, Body, Button, Text } from "native-base";
 
 export default class MapWorker extends Component {
  
+  getUserLocation = () => {
+    navigator.geolocation.getCurrentPosition(position => {
+      console.log(position);
+    }, err => console.log(err));
+  }
 
   render() {
     return (
       <CardItem><Body>
-            <Button block success>
+            <Button block success onPress={this.getUserLocation}>
             <Text>Отсканировать код</Text>
           </Button></Body></CardItem>
     );
