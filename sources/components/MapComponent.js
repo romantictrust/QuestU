aa6c4778407c728aa908ff4e3a277aef410cb798
Vlad Tsirkunov <vladcirkunov598@gmail.com>
2019-03-23 00:36:23 +0300

    Added Google Maps

--- sources/components/MapComponent.js
+++ sources/components/MapComponent.js
@@ -1,20 +1,30 @@
 import React, { Component } from "react";
 import { CardItem, Body } from "native-base";
 import { StyleSheet } from "react-native";
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps'
 
 export default class MapComponet extends Component {
   render() {
     return (
-      <CardItem>
-        <Body style={styles.map}>{/* MAP */}</Body>
+      <CardItem style={styles.mapBox}>
+        <Body style={styles.mapBody}><MapView
+          provider={PROVIDER_GOOGLE}
+          style={styles.map}
+          region={{
+            latitude: 37.78825,
+            longitude: -122.4324,
+            latitudeDelta: 0.015,
+            longitudeDelta: 0.0121,
+          }}
+          showsUserLocation={true}
+        ></MapView></Body>
       </CardItem>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  map: {
-    height: 375,
-    backgroundColor: "pink"
-  }
+const styles = StyleSheet.create({ 
+  mapBox: {height: 375, marginTop:'3%'},
+  mapBody: { ... StyleSheet.absoluteFillObject },
+    map: { ...StyleSheet.absoluteFillObject,}
 });
