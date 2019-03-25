0e83a76cf640940fd751c31f4889abbf9aa0953e
Vlad Tsirkunov <vladcirkunov598@gmail.com>
2019-03-25 16:50:51 +0300

    Path trace (live)

--- sources/components/MapComponent.js
+++ sources/components/MapComponent.js
@@ -1,8 +1,16 @@
 import React, { Component } from "react";
 import { CardItem, Body } from "native-base";
-import { StyleSheet } from "react-native";
+import { StyleSheet, Dimensions } from "react-native";
 import MapView, { PROVIDER_GOOGLE, Marker } from "react-native-maps";
 import MapViewDirections from "react-native-maps-directions";
+
+const {width, height} = Dimensions.get('window')
+
+const SCREEN_HEIGHT = height;
+const SCREEN_WIDTH = width;
+const ASPECT_RATIO = width / height;
+const LATITUDE_DELTA = 0.012;
+const LONGTITUTE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
 
 export default class MapComponet extends Component {
   constructor(){
@@ -10,11 +18,9 @@ export default class MapComponet extends Component {
     this.state = {
       initialPosition: {
         latitude: 0,
-        longitude: 0
-      },
-      finalPosition: {
-        latitude: 0,
-        longitude: 0
+        longitude: 0,
+        latitudeDelta: 0,
+        longitudeDelta: 0
       }
     }
   }
@@ -28,23 +34,26 @@ export default class MapComponet extends Component {
 
       var initialReg = {
         latitude: initLat,
-        longitude: initLon
+        longitude: initLon,
+        latitudeDelta: LATITUDE_DELTA,
+        longitudeDelta: LONGTITUTE_DELTA
       }
       
       this.setState({initialPosition: initialReg})
     }, (err) => alert(JSON.stringify(err)), 
     {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000});
 
-    // Трейс тут связать с построением пути
     this.watchID = navigator.geolocation.watchPosition((position) => {
       var initLat = parseFloat(position.coords.latitude);
       var initLon = parseFloat(position.coords.longitude);
 
       var lastReg = {
         latitude: initLat,
-        longitude: initLon
+        longitude: initLon,
+        latitudeDelta: LATITUDE_DELTA,
+        longitudeDelta: LONGTITUTE_DELTA
       }
-      this.setState({lastReg: lastReg})
+      this.setState({initialPosition: lastReg})
     })
   }
 
@@ -64,14 +73,7 @@ export default class MapComponet extends Component {
           <MapView
             provider={PROVIDER_GOOGLE}
             style={styles.map}
-            region={{
-              latitude: Number(destination.latitude),
-              longitude: Number(destination.longitude),
-
-              // РАЗОБРАТЬСЯ
-              latitudeDelta: Number(0.025),
-              longitudeDelta: Number(0.025)
-            }}
+            region={this.state.initialPosition}
             showUserLocation
             followUserLocation
             loadingEnabled
@@ -79,7 +81,6 @@ export default class MapComponet extends Component {
             scrollEnabled={false}
             rotateEnabled={false}
             mapType="hybrid"
-            // onRegionChange={this.onRegionChange}
           >
             <Marker
               coordinate={{
@@ -89,13 +90,12 @@ export default class MapComponet extends Component {
               title={item.title}
               description={item.location}
             />
-
-            {/* Сделать чтобы путь строился постоянно, 
-            посмотреть на видео с постоянным трейсингом */}
             <MapViewDirections
               origin={this.state.initialPosition}
               destination={destination}
               apikey={GOOGLE_MAPS_APIKEY}
+              // driving walking
+              mode="walking"
               strokeWidth={3}
               strokeColor="red"
             />
