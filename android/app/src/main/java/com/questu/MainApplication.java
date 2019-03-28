f82dc73134c91f8d476c9a0645d8d2fa83192229
Vlad Tsirkunov <vladcirkunov598@gmail.com>
2019-03-28 19:05:11 +0300

    Set camera bundle

--- android/app/src/main/java/com/questu/MainApplication.java
+++ android/app/src/main/java/com/questu/MainApplication.java
@@ -3,6 +3,7 @@ package com.questu;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import org.reactnative.camera.RNCameraPackage;
 import com.airbnb.android.react.maps.MapsPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.facebook.react.ReactNativeHost;
@@ -10,6 +11,7 @@ import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 import com.oblador.vectoricons.VectorIconsPackage;
+
 
 import java.util.Arrays;
 import java.util.List;
@@ -26,6 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNCameraPackage(),
             new MapsPackage(),
             new RNGestureHandlerPackage(),
              new VectorIconsPackage()
